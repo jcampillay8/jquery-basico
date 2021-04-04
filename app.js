8dec0cdc0e471a10760a8a93b9dcdda07c21ea1b
Jaime Campillay <jcampillayworks@gmail.com>
2021-04-04 11:56:06 -0400

    Ejemplos 2 de uso básico de JQuery

--- app.js
+++ app.js
@@ -28,5 +28,19 @@ $(document).ready(function () {
 
     // $("img:last").attr("width","50");
 
-    
+    var parrafo = $('#resultado p');
+
+    $('.btn-primary').click(function(){
+        parrafo.addClass("display-4");
+    })
+
+    $('.btn-danger').click(function(){
+        parrafo.removeClass("display-4");
+    })
+
+    $('.btn-warning').click(function(){
+        parrafo.toggleClass("display-4");
+    })
+
+
 });
